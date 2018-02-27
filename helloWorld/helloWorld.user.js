3d3a5a8635545070572568b0e2a45a8a0fa70e63
thinkAttendee2 <36890954+thinkAttendee2@users.noreply.github.com>
2018-02-27 14:37:45 +0000

    Update helloWorld.user.js

--- helloWorld/helloWorld.user.js
+++ helloWorld/helloWorld.user.js
@@ -45,7 +45,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="Joe Bloggs! ";
+   			dojo.query("span.shareSome-title")[0].textContent="Joe Blogg! ";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
